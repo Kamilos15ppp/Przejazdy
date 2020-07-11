bab10a6cd69f9e84b4be22b345a09858fd9dd155
kamil <kamil.sarga@live.com>
2020-07-11 21:00:44 +0200

    Singing up functionality with parse server, hiding software input by tapping background

--- app/src/main/java/com/example/przejazdy/LoginActivity.java
+++ app/src/main/java/com/example/przejazdy/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,7 +14,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText edtUsernameLogin, edtPasswordLogin;
     private Button btnLogin1, btnSignup1;
@@ -66,29 +67,55 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        btnSignup1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnSignup1.setOnClickListener(LoginActivity.this);
+        btnLogin1.setOnClickListener(LoginActivity.this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btnSignup1:
 
                 Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
                 startActivity(intent);
+                finish();
+                break;
+            case R.id.btnLogin1:
 
-            }
-        });
+
+
+                break;
+
+        }
 
     }
 
     private void showImageToast1(View view) {
 
         Context context = getApplicationContext();
-        CharSequence text = "Tap image for onehand mode";
         int duration = Toast.LENGTH_SHORT;
         int x = imgLoginBus.getTop();
         int y = 350;
-        Toast toast = Toast.makeText(context, text, duration);
+        Toast toast = Toast.makeText(context, R.string.tap_image, duration);
         toast.setGravity(Gravity.TOP, x, y);
         toast.show();
 
     }
 
+    public void rootLayoutTapped(View view) {
+
+        try {
+
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
 }
--- app/src/main/java/com/example/przejazdy/SignupActivity.java
+++ app/src/main/java/com/example/przejazdy/SignupActivity.java
@@ -1,11 +1,13 @@
 package com.example.przejazdy;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -13,7 +15,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignupActivity extends AppCompatActivity {
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText edtUsernameSignup, edtPasswordSignup;
     private Button btnSignup2, btnLogin2;
@@ -65,29 +72,105 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
-        btnLogin2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnLogin2.setOnClickListener(SignupActivity.this);
+        btnSignup2.setOnClickListener(SignupActivity.this);
+
+        if (ParseUser.getCurrentUser() != null) {
+
+            //ParseUser.getCurrentUser().logOut();
+            //transitionSocialMediaActivity();
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btnSignup2:
+
+                if (edtUsernameSignup.getText().toString().equals("") ||
+                        edtPasswordSignup.getText().toString().equals("") ) {
+
+                    FancyToast.makeText(SignupActivity.this,
+                            getString(R.string.fancy_username_pass_required),
+                            Toast.LENGTH_SHORT, FancyToast.INFO,
+                            false).show();
+
+                } else {
+
+                    final ParseUser appUser = new ParseUser();
+                    appUser.setUsername(edtUsernameSignup.getText().toString());
+                    appUser.setPassword(edtPasswordSignup.getText().toString());
+
+                    Context context = SignupActivity.this;
+                    final ProgressDialog progressDialog = new ProgressDialog(context);
+                    progressDialog.setMessage(String.format("%s %s", getString(R.string.progressdialog_signingup), edtUsernameSignup.getText().toString()));
+                    progressDialog.show();
+
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+
+                            if (e == null) {
+
+                                FancyToast.makeText(SignupActivity.this,
+                                        appUser.getUsername() + " " + getString(R.string.fancy_user_signedup),
+                                        Toast.LENGTH_SHORT, FancyToast.SUCCESS,
+                                        false).show();
+                                //transitionPrzejazdy();
+
+                            } else {
+
+                                FancyToast.makeText(SignupActivity.this,
+                                        getString(R.string.fancy_user_signedup_error) + " " + e.getMessage(),
+                                        Toast.LENGTH_LONG, FancyToast.ERROR,
+                                        false).show();
+
+                            }
+
+                            progressDialog.dismiss();
+
+                        }
+                    });
+
+                }
+                break;
+            case R.id.btnLogin2:
 
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
                 startActivity(intent);
+                break;
 
-            }
-        });
+        }
 
     }
 
     private void showImageToast2(View view) {
 
         Context context = getApplicationContext();
-        CharSequence text = "Tap image for onehand mode";
         int duration = Toast.LENGTH_SHORT;
         int x = imgSignupBus.getTop();
         int y = 350;
-        Toast toast = Toast.makeText(context, text, duration);
+        Toast toast = Toast.makeText(context, R.string.tap_image, duration);
         toast.setGravity(Gravity.TOP, x, y);
         toast.show();
 
+    }
+
+    public void rootLayoutTapped(View view) {
+
+        try {
+
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
     }
 
 }
