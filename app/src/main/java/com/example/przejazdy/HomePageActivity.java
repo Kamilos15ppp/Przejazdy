78e2f1c44e899908c2dbf7ea1677daf22003c68d
kamil <kamil.sarga@live.com>
2020-07-11 22:10:16 +0200

    Added tabLayout for fragments

--- app/src/main/java/com/example/przejazdy/HomePageActivity.java
+++ app/src/main/java/com/example/przejazdy/HomePageActivity.java
@@ -2,13 +2,35 @@ package com.example.przejazdy;
 
 import android.os.Bundle;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
+
     }
+
 }
---
+++ app/src/main/java/com/example/przejazdy/TabAdapter.java
@@ -0,0 +1,63 @@
+package com.example.przejazdy;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabAdapter extends FragmentPagerAdapter {
+
+    public TabAdapter(@NonNull FragmentManager fm) {
+
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int tabPosition) {
+
+        switch (tabPosition) {
+
+            case 0:
+                return new AddTransit();
+            case 1:
+                return new Search();
+            case 2:
+                return new Bus();
+            case 3:
+                return new Tram();
+            case 4:
+                return new Relic();
+            default: return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        switch (position) {
+
+            case 0:
+                return "Dodaj przejazd";
+            case 1:
+                return "Wyszukaj";
+            case 2:
+                return "Autobusy";
+            case 3:
+                return "Tramwaje";
+            case 4:
+                return "Zabytki";
+            default: return null;
+
+        }
+        //return super.getPageTitle(position);
+    }
+}
