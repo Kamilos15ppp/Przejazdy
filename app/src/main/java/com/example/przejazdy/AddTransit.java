993fd6fa013f7bc1605335923b3bd2461adb00ed
kamil <kamil.sarga@live.com>
2020-07-12 21:54:57 +0200

    Displaying data form parse server in add_transit activity

--- app/src/main/java/com/example/przejazdy/AddTransit.java
+++ app/src/main/java/com/example/przejazdy/AddTransit.java
@@ -1,63 +1,96 @@
 package com.example.przejazdy;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddTransit#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddTransit extends Fragment {
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class AddTransit extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
+
+    private ListView listView;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter arrayAdapter;
 
     public AddTransit() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddTransit.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddTransit newInstance(String param1, String param2) {
-        AddTransit fragment = new AddTransit();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_transit, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_transit, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        Context context = getContext();
+        arrayAdapter =new ArrayAdapter(context, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setOnItemClickListener(AddTransit.this);
+        listView.setOnItemLongClickListener(AddTransit.this);
+
+
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+
+                if (e == null) {
+
+                    if (users.size() > 0) {
+
+                        for (ParseUser user : users) {
+
+                            arrayList.add(user.getUsername() + "-" +
+                                    user.get("password") + "-" +
+                                    user.getUpdatedAt());
+
+                        }
+
+                        listView.setAdapter(arrayAdapter);
+                        listView.setVisibility(View.VISIBLE);
+
+                    }
+
+                } else {
+
+                    FancyToast.makeText(getContext(),
+                            e.getMessage(),
+                            Toast.LENGTH_LONG, FancyToast.ERROR,
+                            false).show();
+
+                }
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        return false;
     }
 }
